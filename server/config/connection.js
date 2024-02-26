7e6adcf3fa80253a7ad2d8c9cf0b804845d63ebd
Paramesh <142381403+SPwaran@users.noreply.github.com>
2024-02-26 21:39:55 +0530

    Update connection final

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://parameshprogrammer:reacttest@cluster0.hrziqae.mongodb.net/mern-shopping?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://LufrancKousse:LufrancKousse@cluster0.cirfprx.mongodb.net/mern-shopping?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
